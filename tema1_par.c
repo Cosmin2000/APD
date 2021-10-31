d226bbdf78a325a1a83eec2333aeb6b1b2eb893a
Cosmin2000 <57139926+Cosmin2000@users.noreply.github.com>
2021-10-31 21:37:24 +0200

    Add files via upload

--- tema1_par.c
+++ tema1_par.c
@@ -22,107 +22,119 @@ void *f(void *arg) {
     //int cursor,count;
     struct my_arg* data = (struct my_arg*) arg;
     //int i;
-    int start = data->id * (double)data->object_count / data->P;
-    int end = min(((data->id + 1) * (double)data->object_count / data->P), data->object_count);
+    int id = data->id;
+    int P = data->P;
+    int *count = data->count;
+    int *cursor = data->cursor;
+    int object_count = data->object_count;
+    individual *current_generation = data->current_generation;
+    individual *next_generation = data->next_generation; 
+
+
+    int start = id * (double)object_count / P;
+    int end = min(((id + 1) * (double)object_count / P), object_count);
 
     for (int i = start; i < end; ++i) {
 		data->current_generation[i].fitness = 0;
-		data->current_generation[i].chromosomes = (int*) calloc(data->object_count, sizeof(int));
+		data->current_generation[i].chromosomes = (int*) calloc(object_count, sizeof(int));
 		data->current_generation[i].chromosomes[i] = 1;
 		data->current_generation[i].index = i;
-		data->current_generation[i].chromosome_length = data->object_count;
+		data->current_generation[i].chromosome_length = object_count;
 
 		data->next_generation[i].fitness = 0;
-		data->next_generation[i].chromosomes = (int*) calloc(data->object_count, sizeof(int));
+		data->next_generation[i].chromosomes = (int*) calloc(object_count, sizeof(int));
 		data->next_generation[i].index = i;
-		data->next_generation[i].chromosome_length = data->object_count;
+		data->next_generation[i].chromosome_length = object_count;
 	}
 
     pthread_barrier_wait(&barrier);
 
     for (int k = 0; k < data->generations_count; ++k) {
-        if(data->id == 0) {
-            *data->cursor = 0;
+        if(id == 0) {
+            *cursor = 0;
         }
         // compute fitness and sort by it
-        parallel_compute_fitness_function(data->objects, data->current_generation, data->object_count, data->sack_capacity,start,end);
+        parallel_compute_fitness_function(data->objects, data->current_generation, object_count, data->sack_capacity,start,end);
         pthread_barrier_wait(&barrier);
-        if (data->id == 0) {
-            qsort(data->current_generation, data->object_count, sizeof(individual), cmpfunc);    
+        if (id == 0) {
+            qsort(data->current_generation, object_count, sizeof(individual), cmpfunc);    
             //Calculez 30% din numarul de indivizi pentru a-i pastra
-            *data->count = data->object_count * 3 / 10;
+            *count = object_count * 3 / 10;
         }
         pthread_barrier_wait(&barrier);
 
         // keep first 30% children (elite children selection)
-        start = data->id * (double)*data->count / data->P;
-        end = min(((data->id + 1) * (double)*data->count / data->P), data->object_count);
-		for (int i = start; i < end; ++i) {
+        start = id * (double)*count / P;
+        end = min(((id + 1) * (double)*count / P), *count);
+		for (int i = start; i < end; i++) {
 			copy_individual(data->current_generation + i, data->next_generation + i);
 		}
 
-        pthread_barrier_wait(&barrier);
-        if(data->id == 0) {
-		    *data->cursor = *data->count;
+        // pthread_barrier_wait(&barrier);
+        if(id == 0) {
+		    *cursor = *count;
             // Calculez primii 20% din indivizi
-            *data->count = data->object_count * 2 / 10;
+            *count = object_count * 2 / 10;
         }
-        pthread_barrier_wait(&barrier);
+        // pthread_barrier_wait(&barrier);
 
         // mutate first 20% children with the first version of bit string mutation
-        start = data->id * (double)*data->count / data->P;
-        end = min(((data->id + 1) * (double)*data->count / data->P), data->object_count);
+        start = id * (double)*count / P;
+        end = min(((id + 1) * (double)*count / P), *count);
 
-        for (int i = start; i < end; ++i) {
-			copy_individual(data->current_generation + i, data->next_generation + *data->cursor + i);
-			mutate_bit_string_1(data->next_generation + *data->cursor + i, k);
+        for (int i = start; i < end; i++) {
+			copy_individual(data->current_generation + i, data->next_generation + *cursor + i);
+			mutate_bit_string_1(data->next_generation + *cursor + i, k);
 		}
 
-        pthread_barrier_wait(&barrier);
-        if (data->id == 0) {
-            *data->cursor += *data->count;
+        // pthread_barrier_wait(&barrier);
+        if (id == 0) {
+            *cursor += *count;
             //calculez count pentru urmatorul task
-            *data->count = data->object_count * 2 / 10;
+            *count = object_count * 2 / 10;
         }
-        pthread_barrier_wait(&barrier);
+        // pthread_barrier_wait(&barrier);
 
 
         // mutate next 20% children with the second version of bit string mutation
-        start = data->id * (double)*data->count / data->P;
-        end = min(((data->id + 1) * (double)*data->count / data->P), data->object_count);
+        start = id * (double)*count / P;
+        end = min(((id + 1) * (double)*count / P), *count);
 
-        for (int i = start; i < end; ++i) {
-			copy_individual(data->current_generation + i + *data->count, data->next_generation + *data->cursor + i);
-			mutate_bit_string_2(data->next_generation + *data->cursor + i, k);
+        for (int i = start; i < end; i++) {
+			copy_individual(data->current_generation + i + *count, data->next_generation + *cursor + i);
+			mutate_bit_string_2(data->next_generation + *cursor + i, k);
 		}
         pthread_barrier_wait(&barrier);
 
-        if (data->id == 0) {
-            *data->cursor += *data->count;
+        if (id == 0) {
+            *cursor += *count;
             //calculez count pentru urmatorul task
-            *data->count = data->object_count * 3 / 10;
+            *count = object_count * 3 / 10;
             // (if there is an odd number of parents, the last one is kept as such)
-            if (*data->count % 2 == 1) {
-			    copy_individual(data->current_generation + data->object_count - 1, data->next_generation + *data->cursor + *data->count - 1);
-			    (*data->count)--;
+            if (*count % 2 == 1) {
+			    copy_individual(data->current_generation + object_count - 1, data->next_generation + *cursor + *count - 1);
+			    (*count)--;
 		    }
 
         }
-        pthread_barrier_wait(&barrier);
+        // pthread_barrier_wait(&barrier);
 
 
 		// crossover first 30% parents with one-point crossover
-		start = data->id * (double)*data->count / data->P;
-        end = min(((data->id + 1) * (double)*data->count / data->P), *data->count);
+		start = id * (double)*count / P;
+        end = min(((id + 1) * (double)*count / P), *count);
+        if (start % 2 == 1) {
+            end--;
+        }
 
         //Aici apare seg fault la Tread (id 3) La inputs/in4 5 4 .    ULtimul i a fost 1497
-        for (int i = start; i  < end - 1; i += 2) {
-			crossover(data->current_generation + i, data->next_generation + (*data->cursor) + i, k);
+        for (int i = start; i  < end ; i += 2) {
+			crossover(data->current_generation + i, data->next_generation + (*cursor) + i, k);
 		}
 
         pthread_barrier_wait(&barrier);
 
-        if (data->id == 0) {
+        if (id == 0) {
             // switch to new generation
             individual *tmp = NULL;
 		    tmp = data->current_generation;
@@ -131,30 +143,30 @@ void *f(void *arg) {
         }
         pthread_barrier_wait(&barrier);
 
-        start = data->id * (double)data->object_count / data->P;
-        end = min(((data->id + 1) * (double)data->object_count / data->P), data->object_count);
+        start = id * (double)object_count / P;
+        end = min(((id + 1) * (double)object_count / P), object_count);
 
-        for (int i = start; i < end; ++i) {
+        for (int i = start; i < end; i++) {
 			data->current_generation[i].index = i;
 		}
-        pthread_barrier_wait(&barrier);
+        // pthread_barrier_wait(&barrier);
 
-        if (data->id == 0) {
+        if (id == 0) {
             if (k % 5 == 0) {
 			    print_best_fitness(data->current_generation);
 		    }
         }
-        pthread_barrier_wait(&barrier);
+        // pthread_barrier_wait(&barrier);
     }
 
-    start = data->id * (double)data->object_count / data->P;
-    end = min(((data->id + 1) * (double)data->object_count / data->P), data->object_count);
-    parallel_compute_fitness_function(data->objects,data->current_generation, data->object_count, data->sack_capacity,start,end);
+    start = id * (double)object_count / P;
+    end = min(((id + 1) * (double)object_count / P), object_count);
+    parallel_compute_fitness_function(data->objects,data->current_generation, object_count, data->sack_capacity,start,end);
     
     pthread_barrier_wait(&barrier);
 
-    if (data->id == 0) {
-        qsort(data->current_generation, data->object_count, sizeof(individual), cmpfunc);
+    if (id == 0) {
+        qsort(data->current_generation, object_count, sizeof(individual), cmpfunc);
 	    print_best_fitness(data->current_generation);
     }
 
